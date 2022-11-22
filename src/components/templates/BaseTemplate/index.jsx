bf33d64baf6bb9e58cc44d95d716021ddc4c958b
empleoseguro <kevindosmartinfenixzone@gmail.com>
2022-11-22 14:52:22 -0400

    block ad

--- src/components/templates/BaseTemplate/index.jsx
+++ src/components/templates/BaseTemplate/index.jsx
@@ -5,9 +5,20 @@ import Sidebar from '../../molecules/Sidebar'
 const BaseTemplate = ({ children, mostViewed }) => {
     return (
         <div className='container p-0 d-md-flex'>
-            <div className='w-100 mt-5'>{children}</div>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5223902182849072"
+                crossorigin="anonymous"></script>
+            <ins class="adsbygoogle"
+                style="display:block"
+                data-ad-client="ca-pub-5223902182849072"
+                data-ad-slot="3056942636"
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({ });
+            </script>
+            <div className='w-100'>{children}</div>
             <div className='mb-2 me-2 bg-dark'></div>
-            <Sidebar mostViewed={mostViewed}/>
+            <Sidebar mostViewed={mostViewed} />
         </div>
     )
 }
