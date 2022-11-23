1e231d1a0832287d2cdf0204a6a869a4e47cd9b0
empleoseguro <kevindosmartinfenixzone@gmail.com>
2022-11-23 00:45:18 -0400

    ad

---
+++ src/components/molecules/GoogleAd/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const GoogleAd = () => {
+    return (
+        <div>index</div>
+    )
+}
+
+export default GoogleAd
--- src/components/templates/BaseTemplate/index.jsx
+++ src/components/templates/BaseTemplate/index.jsx
@@ -6,12 +6,7 @@ const BaseTemplate = ({ children, mostViewed }) => {
     return (
         <div className='container p-0 d-md-flex'>
             
-<ins class="adsbygoogle"
-     style="display:block; text-align:center;"
-     data-ad-layout="in-article"
-     data-ad-format="fluid"
-     data-ad-client="ca-pub-5223902182849072"
-     data-ad-slot="2448731688"></ins>
+
 
             <div className='w-100'>{children}</div>
             <div className='mb-2 me-2 bg-dark'></div>
