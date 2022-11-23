92af387d86f543e6ae371db4bb067e6adb6936cd
empleoseguro <kevindosmartinfenixzone@gmail.com>
2022-11-22 22:41:50 -0400

    add

--- src/components/templates/BaseTemplate/index.jsx
+++ src/components/templates/BaseTemplate/index.jsx
@@ -6,6 +6,15 @@ const BaseTemplate = ({ children, mostViewed }) => {
     return (
         <div className='container p-0 d-md-flex'>
             
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-5223902182849072"
+     data-ad-slot="2448731688"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
             <div className='w-100'>{children}</div>
             <div className='mb-2 me-2 bg-dark'></div>
             <Sidebar mostViewed={mostViewed} />
