6eb68511ee741d4320ae4888432910b682e50e4f
empleoseguro <kevindosmartinfenixzone@gmail.com>
2022-11-24 09:34:15 -0400

    fix ad yes

--- src/components/molecules/GoogleAd/index.jsx
+++ src/components/molecules/GoogleAd/index.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 
-const GoogleAd = () => {
+const GoogleAd = ({ timeout }) => {
+
+    const [googleInit, setGoogleInit] = useState(null)
+
+    useEffect(() => {
+
+        setGoogleInit(setTimeout(() => {
+            if (typeof window !== 'undefined')
+                (window.adsbygoogle = window.adsbygoogle || []).push({});
+        }, timeout))
+
+        return () => {
+            if (googleInit) clearTimeout(googleInit)
+        }
+    }, [])
+
+
     return (
-        <div>index</div>
+        <div className={classNames}>
+        <ins
+          className="adsbygoogle"
+          style={style || { display: 'block', textAlign: "center" }}
+          data-ad-client={googleAdId}
+          data-ad-slot={slot}
+          data-ad-format={format || "auto"}
+          data-full-width-responsive="true"
+        ></ins>
+      </div>
     )
 }
 
--- src/components/templates/BaseTemplate/index.jsx
+++ src/components/templates/BaseTemplate/index.jsx
@@ -5,12 +5,17 @@ import Sidebar from '../../molecules/Sidebar'
 const BaseTemplate = ({ children, mostViewed }) => {
     return (
         <>
-        <div className='container p-0 d-md-flex'>
-         
-            <div className='w-100'>{children}</div>
-            <div className='mb-2 me-2 bg-dark'></div>
-            <Sidebar mostViewed={mostViewed} />
-        </div>
+            <div className='container p-0 d-md-flex'>
+                <ins className="adsbygoogle"
+                    style={{display:'block'}}
+                    data-ad-client="ca-pub-5223902182849072"
+                    data-ad-slot="7335061001"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+                <div className='w-100'>{children}</div>
+                <div className='mb-2 me-2 bg-dark'></div>
+                <Sidebar mostViewed={mostViewed} />
+            </div>
         </>
     )
 }
