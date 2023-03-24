0992b427697232b9bc3149439bc2a0f5e498ae04
Kevin Martinez <keving28km@gmail.com>
2023-03-23 21:47:04 -0400

    add tag manager

--- src/components/molecules/AlertPolitics/index.jsx
+++ src/components/molecules/AlertPolitics/index.jsx
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button'
 import Cookies from 'universal-cookie'
 import './alertpolitics.css'
 import { Link } from 'react-router-dom'
+import logo from '../../organisms/NavBar/favicon.svg'
 
 
 
@@ -15,49 +16,79 @@ const AlertPolitics = () => {
 
 const CookiesPrivacy = () => {
     const [statusAlert, setstatusAlert] = useState(false)
+    const [modalCookiesConfig, setmodalCookiesConfig] = useState(false)
     const [statusBanner, setstatusBanner] = useState('show')
+    const [visibilityBanner, setvisibilityBanner] = useState(false)
 
     const cookies = new Cookies()
 
-    console.log(cookies.getAll({ path: '/' }))
+    useEffect(() => {
+        if (!localStorage.getItem('cookies-acept')) {
+            setvisibilityBanner(true)
+            console.log('si existe cookies acept')
+        }
+    }, [])
+
+
+    // console.log(cookies.getAll({ path: '/' }))
 
     const aceptCookies = () => {
         localStorage.setItem('cookies-acept', true)
         cookies.set('infolinks_com', 'cookies', { path: '/' });
-
+        window.dataLayer.push({ 'event': 'acept' })
+        console.log(window.dataLayer)
+        console.log('aceptado')
+        setvisibilityBanner(false)
         setstatusBanner('fade')
     }
 
     const denegateCookies = () => {
         cookies.remove('infolinks.com', { path: '/' })
+        console.log('denagado')
         setstatusBanner('fade')
+        setvisibilityBanner(false)
         return
     }
 
     return (
-        !localStorage.getItem('cookies-acept') ?
+        visibilityBanner ?
             <>
-                <Modal.Dialog className="modal d-block fixed-top rounded" tabindex="-1">
+                <div className='modal-banner  fixed-top p-1 w-100' tabIndex="-1">
+                    <div className='px-lg-5 mx-lg-5'>
+                        <div className='container-banner botder-0 d-flex p-0 mt-2 mx-lg-5 flex-column  rounded d-block  bg-light text-dark j
+        ustify-content-center border shadow mh-50'>
+                            <div className='header-banner rounded-top px-4 py-2 text-light d-flex align-items-center justify-content-between'>
+                                <div>
+                                    <img className='logo' src={logo} loading="lazy" alt='logo' /> mpleo seguro
+                                </div>
+                                <div className='fw-bold fs-6 text-light' style={{ cursor: 'pointer' }}>X</div>
+                            </div>
+                            <div className='body-banner px-3 py-2'>
+                                <p className='fw-bold'>Nota informativa</p>
+                                <p>  Nosotros y terceros seleccionados utilizamos cookies o tecnologías similares con fines técnicos y, con su consentimiento, para otras finalidades según se especifica en la <Link className='a-banner' to='about/cookies'>políticas de cookies</Link>.
+                                    Denegar el consentimiento puede provocar que las características relacionadas no estén disponibles.</p>
+                                <p>
+                                    Utilice el botón “Aceptar” para dar su consentimiento. Utilice el botón “Rechazar” o cierre esta nota informativa para
+                                    continuar sin aceptar. Puede desactivarlas y configurar en cualquier momento. <a className='a-banner' onClick={() => setstatusAlert(true)}>ajustes</a>.
+                                </p>
+                            </div>
+                            <div className='footer-banner d-flex justify-content-between mx-3 mb-3'>
+                                <div className=''>
+                                    <button className='btn btn-secondary rounded-pill fw-bold' onClick={() => setmodalCookiesConfig(true)}>Saber más y personalizar</button>
+                                </div>
+                                <div className='d-flex justify-content-around'>
+                                    <button className='btn btn-primary rounded-pill me-3 fw-bold' onClick={aceptCookies}>Aceptar</button>
+                                    <button className='btn btn-primary rounded-pill fw-bold' onClick={denegateCookies}>Rechazar</button>
+                                </div>
+                            </div>
 
-
-                    <Modal.Body className='bg-dark text-light p-4 rounded'>
-                        Al hacer clic en "Aceptar", usted reconoce que ha leído y entendido nuestras <Link className='a-banner' to='about/politics'>políticas de privacidad</Link>.
-                        <br />
-                        así como también consiente el uso de <Link className='a-banner' to='about/cookies'>cookies</Link> de terceros para mejorar su navegación en nuestro sitio. 
-                        {/* <a href='#' onClick={() => setstatusAlert(true)} className='a-banner'>ajustes</a>. */}
-                        <div className='text-end mt-3'>
-                            <Button onClick={aceptCookies}>Aceptar</Button>
-                            <Button className='btn btn-secondary m-2' onClick={denegateCookies}>Rechazar</Button>
                         </div>
-
-                    </Modal.Body>
-
-
-
-
-
-                </Modal.Dialog>
-                <DetailsCookiesPrivacy show={statusAlert} setShow={setstatusAlert} aceptCookies={aceptCookies} denegateCookies={denegateCookies} />
+                    </div>
+                </div>
+                {
+                    modalCookiesConfig ? <DetailsCookiesPrivacy setmodalCookiesConfig={setmodalCookiesConfig} aceptCookies={aceptCookies} denegateCookies={denegateCookies} cookies={cookies} />
+                        : null
+                }
             </>
             : null
     )
@@ -65,44 +96,150 @@ const CookiesPrivacy = () => {
 
 
 
-const DetailsCookiesPrivacy = ({ show, setShow, aceptCookies, denegateCookies, cookies }) => {
-    let mycookies
+const DetailsCookiesPrivacy = ({ aceptCookies, denegateCookies, cookies, setmodalCookiesConfig }) => {
+    const { infolinks_com } = cookies
+
+    const [infolinks, setinfolinks] = useState(null)
+
+    useEffect(() => {
+        setinfolinks(infolinks_com)
+    }, [cookies])
+
+
+
+    const onChangeCookies = (e) => {
+        console.log(e.target.value)
+    }
+
     let setmycookies
     let setCookies
     //asdas
     return (
-        <div
-            className={`modal ${show ? 'd-block' : 'd-none'}`}
-        >
-            <Modal.Dialog >
-                <Modal.Header className='bg-dark text-light'>
-                    <Modal.Title>Resumen de privacidad</Modal.Title>
-                    <div className='fs-5 fw-bold text-danger me-2 ' style={{ cursor: 'pointer' }} onClick={() => setShow(false)}>X</div>
-                </Modal.Header>
+        <div className='modal-banner  fixed-top p-1 w-100' tabIndex="-1">
+            <div className='px-lg-4 mx-lg-4'>
+                <div className='px-lg-5 mx-lg-5'>
+                    <div className='px-lg-5 mx-lg-5'>
+                        <div className='container-banner border-0 d-flex p-0 mt-4 mx-lg-5 flex-column rounded d-block  bg-light text-dark justify-content-center border shadow'>
+                            <div className='header-banner border-bottom border-secondary rounded-top px-5 py-4 fs-5 text-light d-flex align-items-center justify-content-between'>
+                                <div className='d-flex flex-row'>
+                                    <div className='logo-container'>
+                                        <img className='logo' src={logo} loading="lazy" alt='logo' />
 
-                <Modal.Body className='bg-dark text-light'>
-                    <p>Esta web utiliza cookies para que podamos ofrecerte la mejor experiencia de usuario posible. La información de las cookies se almacena en tu navegador y realiza funciones tales como reconocerte cuando vuelves a nuestra web o ayudar a nuestro equipo a comprender qué secciones de la web encuentras más interesantes y útiles.</p>
+                                    </div>mpleo seguro
+                                </div>
+                                <div className='fw-bold fs-5 text-light' style={{ cursor: 'pointer' }}>
+                                    <button className='btn btn-outline-light'>Ver la política de Cookies completa</button>
+                                </div>
+                            </div>
+                            <div className='body-banner'>
+                                <div className='text-banner text-light px-4 pt-2 pb-2'>
+                                    <h1 className='fw-bold mb-2'>Sus opciones de privacidad</h1>
+                                    <p className='text-dark'>  En este panel puede expresar algunas preferencias relacionadas con el tratamiento de sus datos personales.
+                                        Puede revisar y modificar las elecciones realizadas en cualquier momento volviendo a mostrar este panel a través del enlace proporcionado.
+                                        <br />
+                                        Para denegar su consentimiento de las actividades de tratamiento específicas descritas a continuación, cambie los botones de activado
+                                        a desactivado o utilice el botón de “Rechazar todo” y confirme que quiere guardar sus elecciones.</p>
+                                </div>
+                                <div className='buttons-all d-flex justify-content-end my-2 bg-light py-4 shadow-sm'>
+                                    <button className='btn btn-outline-secondary rounded-pill mx-1 fw-bold'
+                                        onClick={() => {
+                                            aceptCookies()
+                                            setmodalCookiesConfig(false)
 
-                    <p>
-                        Esta web utiliza Google Analytics para recopilar información anónima tal como el número de visitantes del sitio, o las páginas más populares.
-                        Dejar esta cookie activa nos permite mejorar nuestra web. Ver
-                        <Link to="about/politics" className='text-primary'> Politicas de privacidad</Link>.
-                    </p>
+                                        }}>Aceptar todo</button>
+                                    <button className='btn btn-outline-secondary rounded-pill mx-1 fw-bold'
+                                        onClick={() => {
+                                            denegateCookies()
+                                            setmodalCookiesConfig(false)
+
+                                        }
+                                        }>Rechazar todo</button>
+                                </div>
 
 
+                                <div className='px-4'>
+                                    <h5 className='text-dark'> Sus preferencias de consentimiento para tecnologías de seguimiento</h5>
+                                    <p className="fs-6">
+                                        Las opciones proporcionadas en esta sección le permiten personalizar sus preferencias de consentimiento para cualesquiera
+                                        tecnologías de seguimiento utilizadas para las finalidades descritas a continuación. Para obtener más información sobre cómo
+                                        nos ayudan estos rastreadores y cómo funcionan, consulte la política de cookies. Por favor tenga en cuenta que denegar el
+                                        consentimiento para una finalidad concreta puede provocar que las características relacionadas no estén disponibles.
+                                    </p>
 
-                    <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked={mycookies} onClick={() => setCookies(!mycookies)} />
-                        <label class="form-check-label" for="flexSwitchCheckChecked">{mycookies ? "activadas" : "desactivadas"}</label>
+                                    <p className="fs-6">
+                                        <div className='title-option d-flex justify-content-between align-items-center mb-2'>
+                                            <label className='fs-5 fw-bold'>
+                                                Necesarios
+                                            </label>
+                                            <div class="form-check form-switch ">
+                                                <input class="checkbox-banner form-check-input" type="checkbox" role="switch" id="flexSwitchCheckCheckedDisabled" disabled checked style={{ width: 60, height: 30 }} />
+
+                                            </div>
+                                        </div>
+                                        <p className="fs-6">Estos rastreadores se utilizan para actividades que son estrictamente necesarias para gestionar o
+                                            prestar el servicio que usted nos ha solicitado y, por tanto, no exigen su consentimiento.</p>
+                                        <div className='title-option d-flex justify-content-between align-items-center mb-2'>
+                                            <label className='fs-5 fw-bold'>
+                                                Funcionalidad
+                                            </label>
+                                            <div class="form-check form-switch ">
+                                                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked style={{ width: 60, height: 30 }} />
+
+                                            </div>
+                                        </div>
+                                        <p className="fs-6">Estos rastreadores hacen posibles interacciones y funcionalidades básicas que le permiten acceder
+                                            a determinadas características de su servicio y facilitan su comunicación con nosotros.</p>
+                                        <div className='title-option d-flex justify-content-between align-items-center mb-2'>
+                                            <label className='fs-5 fw-bold'>
+                                                Experiencia
+                                            </label>
+                                            <div class="form-check form-switch ">
+                                                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked style={{ width: 60, height: 30 }} />
+
+                                            </div>
+                                        </div>
+                                        <p className="fs-6">Estos rastreadores nos ayudan a mejorar la calidad de su experiencia de usuario y permiten la interacción
+                                            con los contenidos, las redes y las plataformas externos.</p>
+                                        <div className='title-option d-flex justify-content-between align-items-center mb-2'>
+                                            <label className='fs-5 fw-bold'>
+                                                Medición
+                                            </label>
+                                            <div class="form-check form-switch ">
+                                                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked style={{ width: 60, height: 30 }} />
+
+                                            </div>
+                                        </div>
+                                        <p className="fs-6">Estos rastreadores nos ayudan a medir el tráfico y a analizar su comportamiento con el fin de mejorar nuestro
+                                            servicio.</p>
+                                        <div className='title-option d-flex justify-content-between align-items-center mb-2'>
+                                            <label className='fs-5 fw-bold'>
+                                                Marketing
+                                            </label>
+                                            <div class="form-check form-switch ">
+                                                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked style={{ width: 60, height: 30 }} />
+
+                                            </div>
+                                        </div>
+                                        <p className="fs-6">Estos rastreadores nos ayudan a entregarle anuncios o contenidos promocionales personalizados, y a medir
+                                            su rendimiento.</p>
+                                    </p>
+                                </div>
+
+                            </div>
+                            <div className='footer-banner d-flex justify-content-between align-items-center pt-3 mx-3 mb-3'>
+                                <div className=''>
+                                    <button className='btn btn-outline-secondary fw-bold' onClick={() => setmodalCookiesConfig(false)}>{'< Volver'}</button>
+                                </div>
+                                <div className='d-flex justify-content-around'>
+                                    <button className='btn text-light rounded-pill me-3 fw-bold button-corp' onClick={aceptCookies}>Guardar y continuar</button>
+
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
-                </Modal.Body>
-
-
-                <Modal.Footer className='bg-dark text-light'>
-                    <Button variant="secondary">Cerrar</Button>
-                    <Button variant="primary">Aceptar</Button>
-                </Modal.Footer>
-            </Modal.Dialog>
+                </div>
+            </div>
         </div>
     )
 }
